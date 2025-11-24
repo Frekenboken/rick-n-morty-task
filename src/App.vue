9c22757b04664d2bc34ee05a69214f76a11e35bd
xiaomi <pavel.ov07@mail.ru>
2025-11-24 17:16:36 +0300

    install pinia, tailwindcss, vuerouter

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import Button from "primevue/button"
   </div>
   <HelloWorld msg="Vite + Vue" />
   <Button>Нажми на меня</Button>
+  <h1 class="text-3xl font-bold underline"> Hello world! </h1>
 </template>
 
 <style scoped>
